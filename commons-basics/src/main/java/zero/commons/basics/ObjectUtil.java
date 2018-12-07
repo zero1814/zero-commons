0823a4406d2d65971e91e8ccc867607cef2afe67
zhanghaiyu <zhanghaiyu@reignwood.com>
2018-12-07 16:03:23 +0800

    增加驼峰命名与下划线的相互转换方法

--- commons-basics/src/main/java/zero/commons/basics/ObjectUtil.java
+++ commons-basics/src/main/java/zero/commons/basics/ObjectUtil.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -106,5 +108,68 @@ public class ObjectUtil {
 			fieldNames[i] = fields[i].getName();
 		}
 		return fieldNames;
+	}
+
+	/**
+	 * 
+	 * 方法: underline2Camel <br>
+	 * 描述: 下划线转驼峰法(默认小驼峰) <br>
+	 * 作者: zhy<br>
+	 * 时间: 2018年12月7日 下午3:50:04
+	 * 
+	 * @param line       源字符串
+	 * @param smallCamel 大小驼峰,是否为小驼峰(驼峰，第一个字符是大写还是小写)
+	 * @return 转换后的字符串
+	 */
+	public static String underline2Camel(String line, boolean... smallCamel) {
+		if (line == null || "".equals(line)) {
+			return "";
+		}
+		StringBuffer sb = new StringBuffer();
+		Pattern pattern = Pattern.compile("([A-Za-z\\d]+)(_)?");
+		Matcher matcher = pattern.matcher(line);
+		// 匹配正则表达式
+		while (matcher.find()) {
+			String word = matcher.group();
+			// 当是true 或则是空的情况
+			if ((smallCamel.length == 0 || smallCamel[0]) && matcher.start() == 0) {
+				sb.append(Character.toLowerCase(word.charAt(0)));
+			} else {
+				sb.append(Character.toUpperCase(word.charAt(0)));
+			}
+			int index = word.lastIndexOf('_');
+			if (index > 0) {
+				sb.append(word.substring(1, index).toLowerCase());
+			} else {
+				sb.append(word.substring(1).toLowerCase());
+			}
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * 
+	 * 方法: camel2Underline <br>
+	 * 描述: 驼峰法转下划线 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2018年12月7日 下午3:53:19
+	 * 
+	 * @param line 源字符串
+	 * @return 转换后的字符串
+	 */
+	public static String camel2Underline(String line, boolean... smallCamel) {
+		if (line == null || "".equals(line)) {
+			return "";
+		}
+		line = String.valueOf(line.charAt(0)).toUpperCase().concat(line.substring(1));
+		StringBuffer sb = new StringBuffer();
+		Pattern pattern = Pattern.compile("[A-Z]([a-z\\d]+)?");
+		Matcher matcher = pattern.matcher(line);
+		while (matcher.find()) {
+			String word = matcher.group();
+			sb.append(word.toLowerCase());
+			sb.append(matcher.end() == line.length() ? "" : "_");
+		}
+		return sb.toString();
 	}
 }
