b02aedabbaf12d79745170efb362d5c7771094e3
zero1814 <zero1814@126.com>
2019-01-05 20:09:35 +0800

    zhy

--- commons-basics/src/main/java/zero/commons/basics/vertify/VerificationHelper.java
+++ commons-basics/src/main/java/zero/commons/basics/vertify/VerificationHelper.java
@@ -7,9 +7,27 @@ import org.slf4j.LoggerFactory;
 
 import zero.commons.basics.ObjectUtil;
 
+/**
+ * 
+ * 类: VerificationHelper <br>
+ * 描述: 验证帮助类 <br>
+ * 作者: zhy<br>
+ * 时间: 2018年12月19日 下午10:30:56
+ */
 public class VerificationHelper {
 	private static final Logger logger = LoggerFactory.getLogger(VerificationHelper.class);
 
+	/**
+	 * 
+	 * 方法: vertifyParam <br>
+	 * 描述: 根据验证类型，验证对象属性值 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2018年12月19日 下午10:31:10
+	 * 
+	 * @param obj  验证对象
+	 * @param type 验证类型
+	 * @return
+	 */
 	public static boolean vertifyParam(Object obj, VertifyType type) {
 		try {
 			Field[] fields = obj.getClass().getDeclaredFields();
