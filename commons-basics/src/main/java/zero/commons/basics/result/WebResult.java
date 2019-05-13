789cf3754483736a17de4e0408034a7d7f32f155
zhanghaiyu <zhanghaiyu@reignwood.com>
2019-05-13 09:22:43 +0800

    zhy

--- commons-basics/src/main/java/zero/commons/basics/result/WebResult.java
+++ commons-basics/src/main/java/zero/commons/basics/result/WebResult.java
@@ -11,107 +11,66 @@ public class WebResult {
 	private String message;
 	private Object obj;
 
-	public static WebResult result(ResultType type, String message) {
-		Integer flag = SUCCESS;
-		if (type == ResultType.ERROR) {
-			flag = ERROR;
-		} else if (type == ResultType.NULL) {
-			flag = NONE;
-		}
-		WebResult _result = new WebResult();
-		_result.setCode(flag);
-		_result.setMessage(message);
-		return _result;
-	}
-
-	public static WebResult result(ResultType type, String message, Object obj) {
-		Integer flag = SUCCESS;
-		if (type == ResultType.ERROR) {
-			flag = ERROR;
-		} else if (type == ResultType.NULL) {
-			flag = NONE;
-		}
-		WebResult _result = new WebResult();
-		_result.setCode(flag);
-		_result.setMessage(message);
-		_result.setObj(obj);
-		return _result;
-	}
-
 	/**
 	 * 
-	 * 方法: success <br>
-	 * 描述: 调用接口成功 <br>
+	 * 方法: result <br>
+	 * 描述: 基础结果集 <br>
 	 * 作者: zhy<br>
-	 * 时间: 2019年5月10日 下午4:24:46
+	 * 时间: 2019年5月13日 上午9:18:31
 	 * 
-	 * @param message
+	 * @param base
 	 * @return
 	 */
-	public static WebResult success(String message) {
+	public static WebResult result(BaseResult base) {
 		WebResult result = new WebResult();
-		result.setCode(SUCCESS);
-		result.setMessage(message);
+		Integer code = changeCode(base.getCode());
+		result.setCode(code);
+		result.setMessage(base.getMessage());
 		return result;
 	}
 
 	/**
 	 * 
-	 * 方法: success <br>
-	 * 描述: 调用接口成功 <br>
+	 * 方法: obj <br>
+	 * 描述: 返回对象结果集 <br>
 	 * 作者: zhy<br>
-	 * 时间: 2019年5月10日 下午4:25:08
+	 * 时间: 2019年5月13日 上午9:18:48
 	 * 
-	 * @param message
-	 * @param obj
+	 * @param entity
 	 * @return
 	 */
-	public static WebResult success(String message, Object obj) {
+	public static WebResult obj(EntityResult<?> entity) {
 		WebResult result = new WebResult();
-		result.setCode(SUCCESS);
-		result.setObj(obj);
-		result.setMessage(message);
+		Integer code = changeCode(entity.getCode());
+		result.setCode(code);
+		result.setMessage(entity.getMessage());
+		result.setObj(entity.getEntity());
 		return result;
 	}
 
 	/**
 	 * 
-	 * 方法: none <br>
-	 * 描述: 访问为空 <br>
+	 * 方法: data <br>
+	 * 描述: 返回对象集合结果集 <br>
 	 * 作者: zhy<br>
-	 * 时间: 2019年5月10日 下午4:24:25
+	 * 时间: 2019年5月13日 上午9:18:58
 	 * 
-	 * @param message
+	 * @param data
 	 * @return
 	 */
-	public static WebResult none(String message) {
+	public static WebResult data(DataResult<?> data) {
 		WebResult result = new WebResult();
-		result.setCode(NONE);
-		result.setMessage(message);
-		return result;
-	}
-
-	/**
-	 * 
-	 * 方法: error <br>
-	 * 描述: 调用接口失败 <br>
-	 * 作者: zhy<br>
-	 * 时间: 2019年5月10日 下午4:24:36
-	 * 
-	 * @param message
-	 * @return
-	 */
-	public static WebResult error(String message) {
-		WebResult result = new WebResult();
-		result.setCode(ERROR);
-		result.setMessage(message);
+		Integer code = changeCode(data.getCode());
+		result.setCode(code);
+		result.setMessage(data.getMessage());
+		result.setObj(data.getData());
 		return result;
 	}
 
 	/**
 	 * 
 	 * 方法: page <br>
-	 * 描述: 分页查询 <br>
+	 * 描述: 分页查询结果集 <br>
 	 * 作者: zhy<br>
 	 * 时间: 2019年5月13日 上午9:11:07
 	 * 
@@ -121,12 +80,35 @@ public class WebResult {
 	public static WebResult page(PageResult<?> page) {
 		WebResult result = new WebResult();
 		if (page.getCode() != ResultType.SUCCESS) {
-			return result(page.getCode(), page.getMessage());
+			return result(page);
 		}
 		Page _page = new Page();
 		_page.setTotal(page.getTotal());
 		_page.setData(page.getData());
 		result.setObj(_page);
+		result.setCode(SUCCESS);
+		result.setMessage(page.getMessage());
+		return result;
+	}
+
+	public static WebResult success(String message) {
+		WebResult result = new WebResult();
+		result.setMessage(message);
+		result.setCode(SUCCESS);
+		return result;
+	}
+
+	public static WebResult error(String message) {
+		WebResult result = new WebResult();
+		result.setMessage(message);
+		result.setCode(ERROR);
+		return result;
+	}
+
+	public static WebResult none(String message) {
+		WebResult result = new WebResult();
+		result.setMessage(message);
+		result.setCode(NONE);
 		return result;
 	}
 
@@ -154,4 +136,13 @@ public class WebResult {
 		this.obj = obj;
 	}
 
+	private static Integer changeCode(ResultType type) {
+		Integer code = ERROR;
+		if (type == ResultType.SUCCESS) {
+			code = SUCCESS;
+		} else if (type == ResultType.NULL) {
+			code = NONE;
+		}
+		return code;
+	}
 }
